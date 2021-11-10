020800203ae53de1f984a0058830bd24fcf0526f
Kristopher Kivutha <kkibwana@gmail.com>
2021-11-10 12:56:42 +0300

    added modules

--- Ch2/modules_start.py
+++ Ch2/modules_start.py
@@ -1,10 +1,12 @@
 # import the math module, which contains features for working with mathematics
-
+import math
 
 # the math module contains lots of pre-built functions
-
+print(f"The square root of 16 is {math.sqrt(16)}")
 
 # in addition to functions, some modules contain useful constants
-
+print(f"Pi is: {math.pi}")
 
 # try some of the math functions for yourself here:
+print(math.pow(2,3))
+print(math.ceil(2.4))
