cf44912ab4d8731672d1e5d5b41bbe395312d8c0
Kristopher Kivutha <kkibwana@gmail.com>
2021-11-10 09:15:50 +0300

    added helloworld solution

--- Ch2/helloworld_start.py
+++ Ch2/helloworld_start.py
@@ -1,3 +1,10 @@
 #
 # Example file for HelloWorld
 #
+def main():
+    print("Hello world")
+    name = input("What is your name? ")
+    print(f"Nice to meet you, {name}")
+
+if __name__ == "__main__":
+    main()
