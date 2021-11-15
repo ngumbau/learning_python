22d0f104dc7e31027dcd113e5833aba20d9dbd9d
Kristopher Kivutha <kkibwana@gmail.com>
2021-11-15 11:07:16 +0300

    fetching internet data

--- Ch5/inetdata_start.py
+++ Ch5/inetdata_start.py
@@ -2,5 +2,13 @@
 # Example file for retrieving data from the internet
 #
 
+import urllib.request
+
+def main():
+  webUrl = urllib.request.urlopen("http://www.google.com")
+  print(f"result code: {webUrl.getcode()}")
+  data = webUrl.read()
+  print(data)
+
 if __name__ == "__main__":
   main()
